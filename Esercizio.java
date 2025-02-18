3cbfda9bfe2809a6e3a7f20139a2b55a17e8f233
ChristianBarile3ID <christianalessandro.barile@marconibari.edu.it>
2025-02-18 18:47:23 +0000

    Consegna

--- Esercizio.java
+++ Esercizio.java
@@ -1,26 +1,95 @@
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
-
-//Import di Classi Java necessarie al funzionamento del programma
 import java.util.Scanner;
+import java.util.Random;
 
-// Classe principale, con metodo main
 class Esercizio {
-    // Il programma parte con una chiamata a main().
-    public static void main(String args[])
-    {
-        //Variabili del programma
-        String nome;
+    private static Random random = new Random();
+    private static Scanner input = new Scanner(System.in);
 
-        //Creo l'oggetto in per l'input da tastiera
-        Scanner in = new Scanner( System.in );
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Random random = new Random();
+        int nAnimali, i, j, n, dimensioneAnimali, contAnimali;
 
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
+        System.out.println("Inserisci il numero di animali nella riserva naturale: ");
+        nAnimali = Integer.parseInt(in.nextLine());
+        contAnimali = 0;
+        int[] animali = new int[nAnimali];
 
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+        for (i = 0; i < animali.length; i++) {
+            dimensioneAnimali = (3 + random.nextInt(7)) * 10;
+            if (contAnimali == 0) {
+                animali[0] = dimensioneAnimali;
+                contAnimali++;
+            } else {
+                if (contAnimali == 1) {
+                    animali[1] = dimensioneAnimali;
+                    contAnimali++;
+                } else {
+                    if (contAnimali > 1) {
+                        contAnimali = inserisciElemento(animali, contAnimali, dimensioneAnimali, i);
+                        j = 0;
+                        while (j < contAnimali) {
+                            if (dimensioneAnimali > animali[j]) {
+                                contAnimali = eliminaElemento(animali, contAnimali, j);
+                            } else {
+                                j++;
+                            }
+                        }
+                    }
+                }
+            }
+            visualizzaAnimali(animali, contAnimali);
+        }
+        n = eliminaDuplicati(animali, contAnimali);
+        visualizzaAnimali(animali, contAnimali);
+    }
+    
+    public static int eliminaDuplicati(int[] animali, int n) {
+        int i, j;
+
+        for (i = 0; i < n - 1; i++) {
+            j = i + 1;
+            while (j < n) {
+                if (animali[i] == animali[j]) {
+                    n = eliminaElemento(animali, n, j);
+                } else {
+                    j = j + 1;
+                }
+            }
+        }
+        
+        return n;
+    }
+    
+    public static int eliminaElemento(int[] animali, int n, int posizione) {
+        int i;
+
+        for (i = posizione; i < n - 1; i++) {
+            animali[i] = animali[i + 1];
+        }
+        n = n - 1;
+        
+        return n;
+    }
+    
+    public static int inserisciElemento(int[] animali, int n, int elemento, int posizione) {
+        int i;
+
+        for (i = n; i > posizione; i--) {
+            animali[i] = animali[i - 1];
+        }
+        animali[posizione] = elemento;
+        n = n + 1;
+        
+        return n;
+    }
+    
+    public static void visualizzaAnimali(int[] animali, int n) {
+        int i;
+
+        for (i = 0; i < n; i++) {
+            System.out.println("Dimensione: " + animali[i]);
+        }
+        System.out.println("------");
     }
 }
-
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
